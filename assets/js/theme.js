114fce5c5125ec590face74724fd025d86ddf64d
vikasraj <vikasraj1911@gmail.com>
2017-11-30 16:55:40 +0530

    navigation link target scroll

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,7 @@
+// loading particles
+particlesJS.load('particles-js', './assets/particles.json');
+
+// making slider parallax
 window.onscroll = function () {
     var wScroll = $(window).scrollTop();
     $('.carousel-item').css({
@@ -5,10 +9,23 @@ window.onscroll = function () {
     })
 }
 
-particlesJS.load('particles-js', './assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-
+// blocking right click on particles
 $('#particles-js').on('contextmenu', function (e) {
     e.preventDefault();
+})
+
+// scroll on arrow click
+$('.scroll-to-down').click(function () {
+    $('html,body').animate({
+        scrollTop : $('.greetings-wrapper').offset().top - 150
+    }, 1000)
+})
+
+// scroll to destination when nav link is click
+$('.nav-link').click(function () {
+    var t = $(this).attr('data-target');
+    // history.pushState({}, "page"+($('.nav-link').index($(this))+1), (t).slice(1)); // changing url based on content; == giving error on reload
+    $('html,body').animate({
+        scrollTop : $(t).offset().top - 100
+    }, 1200)
 })
