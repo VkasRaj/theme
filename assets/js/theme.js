f3cf62338a78d2dce0ea2f938194513cfd9a1cd1
vikasraj <vikasraj1911@gmail.com>
2017-11-30 15:20:04 +0530

    added: particles js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,10 @@
-
 window.onscroll = function () {
     var wScroll = $(window).scrollTop();
     $('.carousel-item').css({
         'background-position' : 'center -' + wScroll/5 + 'px'
     })
 }
+
+particlesJS.load('particles-js', './assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
